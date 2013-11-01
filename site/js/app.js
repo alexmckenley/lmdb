d4dca9f1b3b06cddbfd80606d42dda1ac4a8c919
Alex Taylor <alexmckenley@gmail.com>
2013-11-01 16:37:39 -0700

    Updated Marionette views

--- site/js/app.js
+++ site/js/app.js
@@ -10,30 +10,40 @@ App.on('start', function(options) {
     main: "#app",
     rightside: '#rightside'
   });
+  var movieListView = new App.MovieListView({
+    collection: options.movies
+  });
+  App.main.show(movieListView);
   Backbone.history.start();
 });
 
-var TodoApp = new (Backbone.Router.extend({
-  routes: { "": "index",
-  		    "movies/:id": "show" }, // This route does not work at all yet
+// var TodoApp = new (Backbone.Router.extend({
+//   routes: { "": "index",
+//   		    "movies/:id": "show" }, // This route does not work at all yet
 
-  initialize: function(){
-    this.movieList = new MovieList();
-    this.movieListView = new MovieListView({collection: this.movieList});
-    $('#app').html(this.movieListView.el);
-  },
-  start: function(){
-    Backbone.history.start({pushState: true});
-  },
-  index: function(){
-    this.movieList.fetch();
-  },
-  show: function(id){
-    //this.todoList.focusOnTodoItem(id);
-  }
-}));
+//   initialize: function(){
+//     this.movieList = new MovieList();
+//     this.movieListView = new MovieListView({collection: this.movieList});
+//     $('#app').html(this.movieListView.el);
+//   },
+//   start: function(){
+//     Backbone.history.start({pushState: true});
+//   },
+//   index: function(){
+//     this.movieList.fetch();
+//   },
+//   show: function(id){
+//     //this.todoList.focusOnTodoItem(id);
+//   }
+// }));
 
 $(document).ready(function(){
-	App.start();
+  var movies = {[ 
+    { title: 'Wet Cat' },
+    { title: 'Bitey Cat' },
+    { title: 'Surprised Cat' }
+    
+  ]};
+	App.start({movies: movies});
 	console.log("Started");
 });
--- site/js/mvc.js
+++ site/js/mvc.js
@@ -1,6 +1,5 @@
-
 ///// MODELS /////
-window.Movie = Backbone.Model.extend({
+App.Movie = Backbone.Model.extend({
 	defaults: {
 		poster: "img/placeholder.png",
 		title: "No title",
@@ -11,82 +10,24 @@ window.Movie = Backbone.Model.extend({
 
 
 ///// COLLECTIONS /////
-
-window.MovieList = Backbone.Collection.extend({
-  model: Movie,
+App.MovieList = Backbone.Collection.extend({
+	model: Movie,
 	url: '/movies'
 });
 
 
 
 ///// VIEWS /////
-window.MovieDisplayView = Backbone.View.extend({
-	el: '#rightside',
-	template: _.template($('#movieDisplay').html()),
-	initialize: function (){
-		this.model = new app.Movie({
-			poster: "<-----",
-			title: "Please select a movie",
-			year: "<-----"
-		});
-		
-		//this.render();
-		
-		this.listenTo( this.model, 'change', this.render );
-	},
-	
-	render: function() {
-		//console.log("Rendered Display");
-		//console.log(this.model);
-		this.$el.html( this.template( this.model.toJSON() ) );
-
-		return this;
-	}
-	
-});
-
-window.MovieListEntryView = Backbone.View.extend({
+App.MovieListEntryView = Backbone.Marionette.ItemView.extend({
 	tagName: 'div',
 	className: 'movieListEntry',
-	template: _.template($('#movieListEntry').html()),
-	events: {"click" : "updateDisplay"},
-	render: function(){
-		this.$el.html( this.template( this.model.toJSON() ) );
-		//console.log(this);
-		return this;
-	},
-	updateDisplay: function (){
-		//console.log(this.model);
-		app.rightSide.model.set(this.model.toJSON());
-	}
+	template: '#movieListEntry'
 });
 
-window.MovieListView = Backbone.View.extend({
-	el: '#movies',
-	
-	initialize: function (){
-		this.collection = new app.MovieList();
-		this.collection.fetch({reset: true});
-		this.render();
-		//console.log("MLV");
-		
-		this.listenTo( this.collection, 'add', this.renderMovieListEntry );
-		this.listenTo( this.collection, 'reset', this.render );
-	},
-	
-	render: function() {
-		this.collection.each(function( item ){
-			this.renderMovieListEntry( item );
-		}, this );
-		//console.log(this.collection);
-	},
-	
-	renderMovieListEntry: function( item ){
-		var movieListEntryView = new app.MovieListEntryView({
-			model: item
-		});
-		this.$el.append( movieListEntryView.render().el );
+App.MovieListView = Backbone.Marionette.CompositeView.extend({
+	el: '#main',
+	itemView: App.MovieListEntryView,
+	appendHtml: function(collectionView, itemView) {
+		collectionView.$("#app").append(itemView.el);
 	}
-	
 });
-
